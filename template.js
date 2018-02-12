e0e5ba38be62446ec9f2bd880bfeabbf6bbbdc4e
Anita's WorkBook <31773988+Anita7ITP@users.noreply.github.com>
2018-02-12 03:08:56 -0500

    Update template.js

--- template.js
+++ template.js
@@ -1,7 +1,7 @@
-var data = {people: [{name: "Shawn", other: "blah"}, {name: "Joe", other: "No"}]};
+var data = {people: [{name: "Amanda", other: "Kate"}, {name: "Steven", other: "King"}{name: "Kim", other: "Klein"},{name: "Anita", other: "Ruth"},{name: "Judith", other: "Ben"},{name: "Neil", other: "Omarion"},]};
 
 
 app.get('/templatetest', function(req, res) {
-	var data = {person: {name: "Shawn", other: "blah"}};
+	var data = {person: {name: "Amanda", other: "Kate"}};
     res.render('template.ejs', data);
 });
